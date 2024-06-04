370346558b8587b273e1f03cca8deb41c335883a
Logan Baucum <33470316+LoganBaucum@users.noreply.github.com>
2024-06-04 15:29:50 -0500

    Update NewProject.jsx
    Added validation on Save when creating a new project.
    Added the Modal component to appear if the input is wrong.

--- src/Components/NewProject.jsx
+++ src/Components/NewProject.jsx
@@ -1,7 +1,10 @@
 import { useRef } from "react";
-import Input from "./Input";
+import Input from "./Input.jsx";
+import Modal from "./Modal.jsx";
 
 export default function NewProject({ onAddProject }) {
+  const modalRef = useRef();
+
   const titleRef = useRef();
   const descriptionRef = useRef();
   const dueDateRef = useRef();
@@ -11,8 +14,14 @@ export default function NewProject({ onAddProject }) {
     const enteredDescription = descriptionRef.current.value;
     const enteredDueDate = dueDateRef.current.value;
 
-    // TODO: Validation ...
-
+    if (
+      enteredTitle.trim() === "" ||
+      enteredDescription.trim() === "" ||
+      enteredDueDate.trim() === ""
+    ) {
+      modalRef.current.open();
+      return;
+    }
     onAddProject({
       title: enteredTitle,
       descrption: enteredDescription,
@@ -21,31 +30,41 @@ export default function NewProject({ onAddProject }) {
   }
 
   return (
-    <div className="w-[35rem] mt-16">
-      <menu className="flex items-center justify-end gap-4 my-4">
-        <li>
-          <button className="text-stone-800 hover:text-stone-950">
-            Cancel
-          </button>
-        </li>
-        <li>
-          <button
-            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
-            onClick={handleSave}
-          >
-            Save
-          </button>
-        </li>
-      </menu>
-      <div>
-        <Input type="text" ref={titleRef} label="Title" />
+    <>
+      <Modal ref={modalRef} buttonCaption="Close">
+        <h2 className="text-xl font-bold text-stone-800 my-4">
+          Invalid Input.
+        </h2>
+        <p className="text-stone-500 my-4">
+          Make sure you proivide a valid value for every input.
+        </p>
+      </Modal>
+      <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button className="text-stone-800 hover:text-stone-950">
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+              onClick={handleSave}
+            >
+              Save
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Input type="text" ref={titleRef} label="Title" />
+        </div>
+        <div>
+          <Input ref={descriptionRef} label="Description" textArea />
+        </div>
+        <div>
+          <Input type="date" ref={dueDateRef} label="Due Date" />
+        </div>
       </div>
-      <div>
-        <Input ref={descriptionRef} label="Description" textArea />
-      </div>
-      <div>
-        <Input type="date" ref={dueDateRef} label="Due Date" />
-      </div>
-    </div>
+    </>
   );
 }
