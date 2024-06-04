525bfa4e57d83846afd9d2e6260928093de08d93
Logan Baucum <33470316+LoganBaucum@users.noreply.github.com>
2024-06-04 13:11:41 -0500

    Update App.jsx
    Added Sidebar to the app.
    Created placeholder projects info state.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
+import { useState } from "react";
+import Sidebar from "./Components/Sidebar";
+
 function App() {
+  const [projects, setProjects] = useState([
+    {
+      title: "Project 1",
+      description: "placeholder text",
+      dueDate: "2024-04-06",
+      tasks: ["Task 1", "Task 2", "Task 3"],
+    },
+    {
+      title: "Project 2",
+      description: "some more text thats placeholder",
+      dueDate: "2024-04-26",
+      tasks: ["Task 1", "Task 2"],
+    },
+  ]);
+
+  const [selectedProject, SetSelectedProject] = useState();
+
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8">
+      <Sidebar />
+    </main>
   );
 }
 
