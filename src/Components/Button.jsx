9e9c0970e5ac03370698af9757a4ea24a3719b6f
Logan Baucum <33470316+LoganBaucum@users.noreply.github.com>
2024-06-04 13:50:02 -0500

    Update Button.jsx
    Fixed bug with variable declaration of styledClasses.

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,5 +1,5 @@
 export default function Button({ children, ...props }) {
-  styledclasses =
+  const styledclasses =
     "px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:stroke-stone-600 hover:text-stone-100";
   return (
     <button className={styledclasses} {...props}>
