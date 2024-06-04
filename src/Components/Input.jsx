980f2a1400261777d505c769720dd34be317d8c8
Logan Baucum <33470316+LoganBaucum@users.noreply.github.com>
2024-06-04 13:29:42 -0500

    Styled the Input component

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -1,11 +1,19 @@
 export default function Input({ label, textArea, ...props }) {
+  const styleClasses =
+    "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600";
+
   return (
-    <p>
-      <label htmlFor={label}>{label}</label>
+    <p className="flex flex-col gap-1 my-4">
+      <label
+        className="text-sm font-bold uppercase text-stone-500"
+        htmlFor={label}
+      >
+        {label}
+      </label>
       {textArea ? (
-        <textarea name={label} {...props} />
+        <textarea className={styleClasses} name={label} {...props} />
       ) : (
-        <input name={label} {...props} />
+        <input className={styleClasses} name={label} {...props} />
       )}
     </p>
   );
