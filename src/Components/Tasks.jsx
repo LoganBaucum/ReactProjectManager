b629528fa09333cb86e0d6840e55039711ceb1a2
Logan Baucum <33470316+LoganBaucum@users.noreply.github.com>
2024-06-04 18:31:42 -0500

    Update Tasks.jsx
    Removed console logs

--- src/Components/Tasks.jsx
+++ src/Components/Tasks.jsx
@@ -7,10 +7,6 @@ export default function Tasks({
   onAddTask,
   onDeleteTask,
 }) {
-  console.log(tasks.length);
-  tasks.map((task) => {
-    console.log(task.text);
-  });
   return (
     <section>
       <h2 className="text-2xl font-bold text-stone-700 mb-4">Tasks</h2>
